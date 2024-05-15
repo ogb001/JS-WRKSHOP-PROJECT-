6d16091733919e6a8845ec431bf6f0ab32af73f1
ogb001 <162340330+ogb001@users.noreply.github.com>
2024-05-15 17:57:55 +0100

    Add files via upload

---
+++ JS WRKSHOP/script.js
@@ -0,0 +1,24 @@
+// var x = "google"
+// x += "." + "com"
+
+// console.log(x)
+
+// function convert(minutes) {
+// 	seconds = minutes * 60
+// 	return seconds
+
+//     console.log(convert(minutes))
+// }
+
+
+function calcAge(ageyears) {
+	var agedays = ageyears * 365
+	return agedays;
+}
+
+function findPerimeter(length, width) {
+	return (length *2) + (width * 2)
+}
+
+https://edabit.com/challenge/wAdE9te55cowBLcPs
+https://edabit.com/challenge/wAdE9te55cowBLcPs
